29a87a6a5504864e5ec7a2786492f13bee18118c
MIYABE Tatsuhiko <tatsuhiko@miya.be>
2022-10-29 04:51:38 +0000

    initial import

--- pdfg2d-demo/src/main/java/net/zamasoft/pdfg2d/demo/DrawPdfApp.java
+++ pdfg2d-demo/src/main/java/net/zamasoft/pdfg2d/demo/DrawPdfApp.java
@@ -6,10 +6,10 @@ import java.io.File;
 
 import net.zamasoft.pdfg2d.PdfGraphics2D;
 
-public class DrawPDFApp {
+public class DrawPdfApp {
 
 	public static void main(String[] args) throws Exception {
-		try(PdfGraphics2D g2d = new PdfGraphics2D(new File("out.pdf"))) {
+		try(PdfGraphics2D g2d = new PdfGraphics2D(new File("out/drawpdf.pdf"))) {
 			g2d.setColor(Color.RED);
 			g2d.setStroke(new BasicStroke(5));
 			g2d.drawOval(100, 100, 200, 200);
